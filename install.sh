60cf7285deebb7e6efd5737db8790c9362e7d922
Anders Dahl <dahl.a@husky.neu.edu>
2014-04-29 02:40:34 -0400

    Removed vundle update in install and autoindented code

--- install.sh
+++ install.sh
@@ -5,53 +5,42 @@ dotfiles="$HOME/.dotfiles"
 # to error out
 warn() {
     echo "$1" >&2
-  }
+}
 
-  die() {
-      warn "$1"
-        exit 1
-      }
+die() {
+    warn "$1"
+    exit 1
+}
 
-      lnif() {
-          if [ ! -e $2 ] ; then
-            ln -s $1 $2
-              fi
-            }
+lnif() {
+    if [ ! -e $2 ] ; then
+        ln -s $1 $2
+    fi
+}
 
-            echo "Installing/Updating dotfiles...\n"
+echo "Installing/Updating dotfiles...\n"
 
-            if [ ! -e $dotfiles/.git ]; then
-              echo "Cloning dotfiles\n"
-                git clone https://github.com/AndersOLDahl/dotfiles.git $dotfiles
-              else
-                echo "Updating dotfiles\n"
-                  cd $dotfiles && git pull
-                fi
+if [ ! -e $dotfiles/.git ]; then
+    echo "Cloning dotfiles\n"
+    git clone https://github.com/AndersOLDahl/dotfiles.git $dotfiles
+else
+    echo "Updating dotfiles\n"
+    cd $dotfiles && git pull
+fi
 
-                # zsh
-                echo "Setting up bash...\n"
-                lnif $dotfiles/zshrc $HOME/.zshrc
+# zsh
+echo "Setting up bash...\n"
+lnif $dotfiles/zshrc $HOME/.zshrc
 
-                # git
-                echo "Setting up git...\n"
-                lnif $dotfiles/gitconfig $HOME/.gitconfig
-                lnif $dotfiles/gitignore_global $HOME/.gitignore_global
+# git
+echo "Setting up git...\n"
+lnif $dotfiles/gitconfig $HOME/.gitconfig
+lnif $dotfiles/gitignore_global $HOME/.gitignore_global
 
-                # vim
-                echo "Setting up vim...\n"
-                lnif $dotfiles/vimrc $HOME/.vimrc
-                lnif $dotfiles/vim $HOME/.vim
+# vim
+echo "Setting up vim...\n"
+lnif $dotfiles/vimrc $HOME/.vimrc
+lnif $dotfiles/vim $HOME/.vim
 
-                # vim vundle
-                if [ ! -d $dotfiles/vim/bundle ]; then
-                  mkdir -p $dotfiles/vim/bundle
-                fi
-
-                if [ ! -e $dotfiles/vim/bundle/vundle ]; then
-                  echo "Installing vundle"
-                    git clone http://github.com/gmarik/vundle.git
-                    $dotfiles/vim/bundle/vundle
-                  fi
-
-                  echo "Update/Install plugins using vundle"
-                  vim +PluginInstall +qall
+echo "Update/Install plugins using vundle"
+vim +PluginInstall +qall
